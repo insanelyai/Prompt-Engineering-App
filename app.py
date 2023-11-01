614d8eb4c297d15b938a227eba4c0f98516ed0e0
Hasan Sayyed <hasansayyed.personal@gmail.com>
2023-11-02 02:00:56 +0530

    Update: Connected with API

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 import openai
 
+openai.api_key = "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-ctchp"
 
 app = Flask(__name__)
 
@@ -11,8 +12,23 @@ def home():
         input = request.form['userInput']
         prompt = request.form['prompt']
 
-        print(input + prompt)
-    return render_template('template.html')
+        post_req = input + " " + prompt
+
+        # ChatGPT Training Model
+        model_engine = "text-davinci-002"
+        completions = openai.Completion.create(
+            engine=model_engine,
+            prompt=post_req,
+            max_tokens=100,
+        )
+
+        # Get the response
+        get_res = completions.choices[0].text.strip()
+
+        fetched_data = get_res
+        return render_template('template.html', fetched_data=fetched_data)
+    else:
+        return render_template('template.html')
 
 
 if __name__ == '__main__':
